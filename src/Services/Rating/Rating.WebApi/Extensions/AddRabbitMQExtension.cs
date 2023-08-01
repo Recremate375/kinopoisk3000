4fb5dbcb8ba8347507bfa6446547eb0507e4b5da
Alexey <70941526+Recremate375@users.noreply.github.com>
2023-08-01 13:01:43 +0300

    Commit#01.08.2023
    Add some sections to docker-compose file, add information to rabbitMQ to appsettings.json

--- src/Services/Rating/Rating.WebApi/Extensions/AddRabbitMQExtension.cs
+++ src/Services/Rating/Rating.WebApi/Extensions/AddRabbitMQExtension.cs
@@ -5,18 +5,24 @@ namespace Rating.WebApi.Extensions
 {
 	public static class AddRabbitMQExtension
 	{
-		public static IServiceCollection ConfigureMassTransit(this IServiceCollection services)
+		public static IServiceCollection ConfigureMassTransit(this IServiceCollection services, IConfiguration configuration)
 		{
 			services.AddMassTransit(config =>
 			{
-				config.SetEndpointNameFormatter(
-					new KebabCaseEndpointNameFormatter(prefix: "film", includeNamespace: false));
-
 				config.AddConsumers(typeof(FilmConsumer).Assembly);
+
+				var host = configuration["RabbitMQ:Host"];
+				var virtualHost = configuration["RabbitMQ:VirtualHost"];
+				var username = configuration["RabbitMQ:Username"];
+				var password = configuration["RabbitMQ:Password"];
 
 				config.UsingRabbitMq((context, config) =>
 				{
-					config.Host("rabbitmq", "/");
+					config.Host(host, virtualHost, h =>
+					{
+						h.Username(username);
+						h.Password(password);
+					});
 					config.ConfigureEndpoints(context);
 				});
 			});
--- src/Services/Rating/Rating.WebApi/Program.cs
+++ src/Services/Rating/Rating.WebApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMSSqlDbContext(builder.Configuration);
 builder.Services.AddRepositories();
 builder.Services.AddServices();
-builder.Services.ConfigureMassTransit();
+builder.Services.ConfigureMassTransit(builder.Configuration);
 builder.Services.ConfigreFluentValidation();
 builder.Services.ConfigureAutoMapper();
 
